45ad6ea14b70e942871e6098dc947c9089dd020e
Christina L. Cole <christinalcole@gmail.com>
2017-03-06 18:54:56 -0500

    reset initial test condition

--- benchmark_test.rb
+++ benchmark_test.rb
@@ -4,7 +4,7 @@ require './quicksort1.rb'
 require './quicksort2.rb'
 
 arr = [4, 10, 1, 18, 2]
-#arr = Array.new(20) {rand(1..100)}  #second example, to show limitations v. native sort
+# arr = Array.new(20) {rand(1..100)}  #second example, to show limitations v. native sort
 
 Benchmark.bm do |bm|
   bm.report('quicksort1'){inplace_quicksort(arr, 0, arr.length-1)}
