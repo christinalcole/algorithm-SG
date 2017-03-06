19d0a22e02b48233b5b1e161cd9e0b633b71d0f2
Christina L. Cole <christinalcole@gmail.com>
2017-03-05 21:40:30 -0500

    benchmark quicksort versions v. native sort

---
+++ benchmark_test.rb
@@ -0,0 +1,13 @@
+require 'benchmark'
+
+require './quicksort1.rb'
+require './quicksort2.rb'
+
+arr = [4, 10, 1, 18, 2]
+#arr = Array.new(20) {rand(1..100)}  #second example, to show limitations v. native sort
+
+Benchmark.bm do |bm|
+  bm.report('quicksort1'){inplace_quicksort(arr, 0, arr.length-1)}
+  bm.report('quicksort2'){arr.quicksort}
+  bm.report('native sort'){arr.sort}
+end
