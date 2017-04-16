3d337e4a8f10eee315943939aad5ec5dee938a0d
Christina L. Cole <christinalcole@gmail.com>
2017-04-16 15:25:51 -0400

    add mergesort implementation for comparison

--- benchmark_test.rb
+++ benchmark_test.rb
@@ -2,6 +2,7 @@ require 'benchmark'
 
 require_relative './quicksort/quicksort1.rb'
 require_relative './quicksort/quicksort2.rb'
+require_relative './mergesort/mergesort.rb'
 
 arr = [4, 10, 1, 18, 2]
 # arr = Array.new(20) {rand(1..100)}  #second example, to show limitations v. native sort
@@ -9,5 +10,6 @@ arr = [4, 10, 1, 18, 2]
 Benchmark.bm do |bm|
   bm.report('quicksort1'){inplace_quicksort(arr, 0, arr.length-1)}
   bm.report('quicksort2'){arr.quicksort}
+  bm.report('mergesort'){mergesort(arr)}
   bm.report('native sort'){arr.sort}
 end
