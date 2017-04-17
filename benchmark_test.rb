1920906bc465294f68cd9dbacf2288d8de954e82
Christina L. Cole <christinalcole@gmail.com>
2017-04-16 21:59:35 -0400

    add class-level mergesort implementation to benchmark

--- benchmark_test.rb
+++ benchmark_test.rb
@@ -3,6 +3,7 @@ require 'benchmark'
 require_relative './quicksort/quicksort1.rb'
 require_relative './quicksort/quicksort2.rb'
 require_relative './mergesort/mergesort.rb'
+require_relative './mergesort/mergesort2.rb'
 
 arr = [4, 10, 1, 18, 2]
 # arr = Array.new(20) {rand(1..100)}  #second example, to show limitations v. native sort
@@ -11,5 +12,6 @@ Benchmark.bm do |bm|
   bm.report('quicksort1'){inplace_quicksort(arr, 0, arr.length-1)} #Comment out this line to test larger arrays
   bm.report('quicksort2'){arr.quicksort}
   bm.report('mergesort'){mergesort(arr)}
+  bm.report('mergesort2'){arr.mergesort}
   bm.report('native sort'){arr.sort}
 end
