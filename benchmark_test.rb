2b436de701991d54460e99c4a9a2617bf355bf08
Christina L. Cole <christinalcole@gmail.com>
2017-04-16 21:59:56 -0400

    add clarity to benchmark display

--- benchmark_test.rb
+++ benchmark_test.rb
@@ -7,6 +7,8 @@ require_relative './mergesort/mergesort2.rb'
 
 arr = [4, 10, 1, 18, 2]
 # arr = Array.new(20) {rand(1..100)}  #second example, to show limitations v. native sort
+print arr
+print "\n"
 
 Benchmark.bm do |bm|
   bm.report('quicksort1'){inplace_quicksort(arr, 0, arr.length-1)} #Comment out this line to test larger arrays
